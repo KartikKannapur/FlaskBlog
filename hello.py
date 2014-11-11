f6af3721bf9761b1011c8433b15bb31e6bdfc913
Kartik Kannapur <kartikkannapur@gmail.com>
2014-11-12 01:49:02 +0530

    Basic Templates Added

--- hello.py
+++ hello.py
@@ -5,7 +5,6 @@
 
 from flask import Flask
 from flask import render_template
-from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 
@@ -17,7 +16,13 @@ def index():
 def user(name):
     return render_template('user.html',name=name)
     
-bootstrap = Bootstrap(app)
-    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
