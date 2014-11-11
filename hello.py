aad1571cb6a4023a2700102171b6a7309870d14f
Kartik Kannapur <kartikkannapur@gmail.com>
2014-11-12 01:22:04 +0530

    Bootstrap Added

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@
 
 from flask import Flask
 from flask import render_template
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 
@@ -12,10 +13,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name)
+    
+bootstrap = Bootstrap(app)
     
 if __name__ == '__main__':
     app.run(debug=True)
