abbe03c5819002bf42ea2db12997058ad4973876
Kartik Kannapur <kartikkannapur@gmail.com>
2014-11-12 19:23:31 +0530

    Registration Added

--- hello.py
+++ hello.py
@@ -3,11 +3,16 @@
 ###################
 
 
-from flask import Flask
+from flask import  Flask, session, redirect, url_for
 from flask import render_template
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import TextField, PasswordField, validators, HiddenField
+from wtforms import TextAreaField, BooleanField
+from wtforms.validators import Required, EqualTo, Optional
+from wtforms.validators import Length, Email
+
+
 
 
 app = Flask(__name__)
@@ -31,20 +36,27 @@ def internal_server_error(e):
 
 #User Sign Up Form
 class signUpForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
+    email = TextField('Email address', validators=[
+    	Required('Please provide a valid email address'),
+        Length(min=6, message=(u'Email address too short')),
+        Email(message=(u'That\'s not a valid email address.'))])
+    
+    password = PasswordField('Pick a secure password', validators=[
+    	Required(),
+        Length(min=6, message=(u'Please give a longer password'))])
+    
+    username = TextField('Choose your username', validators=[Required()])
+    
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	name = None
+	username = None
  	form = signUpForm()
-
  	if form.validate_on_submit():
- 		name = form.name.data
- 		form.name.data = ''
- 	return render_template('index.html', form=form, name=name)
+ 		session['username'] = form.username.data
+ 		return redirect(url_for('index'))
 
-
+ 	return render_template('index.html', form=form, name=session.get('username'))
 
 if __name__ == '__main__':
     app.run(debug=True)
