90c35475b9dec80a12646800c75e803d0bb32996
Kartik Kannapur <kartikkannapur@gmail.com>
2014-11-12 15:38:29 +0530

    Basic Templates Updated

--- hello.py
+++ hello.py
@@ -5,6 +5,9 @@
 
 from flask import Flask
 from flask import render_template
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -23,6 +26,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+#User Registation Form
+class registrationForm(Form):
+    name = StringField('name', validators=[DataRequired()])
 
 if __name__ == '__main__':
     app.run(debug=True)
